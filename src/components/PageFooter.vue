6aeafbd38545b646e42dd8de29273df0fefe28ff
CarloSalaris <carloa.salaris@gmail.com>
2023-06-13 18:13:00 +0200

    PageFooter background

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -20,12 +20,25 @@ export default {
     <footer class="bg_blue">
         <div class="container_centered">
             <NavContainer btnColor="btn_3" :menuElements="store.navMenu.elements" />
+
             <!-- LEFT BLOCK -->
-            <!-- TITLE slogan -->
-            <!-- BUTTON 1 -->
-            <!-- Copyright -->
+
+            <div>
+                <!-- TITLE slogan -->
+                <h4>SLOGAN</h4>
+                <!-- BUTTON 1 -->
+                <button class="btn_1">What is a VA exactly?</button>
+                <!-- Copyright -->
+            </div>
         </div>
     </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+    height: 655px;
+    background-image: url("../assets/images/bg/footer-bg-1.svg");
+    background-size: cover;
+    background-position: center;
+}
+</style>
