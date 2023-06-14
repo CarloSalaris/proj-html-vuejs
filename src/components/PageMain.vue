3f526a9c8ae428db851961bf4e7f9e8168e77c42
CarloSalaris <carloa.salaris@gmail.com>
2023-06-14 09:30:34 +0200

    PageMain - Services section top grid

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -9,15 +9,68 @@ export default {
 
 <template>
     <main>
-        <section>'Services' section</section>
+        <section>
+            <div class="container_centered">
+                <div class="servicesGrid">
+                    <div class="services"> Available services</div>
+                    <div class="servicesCards">
+                        <!-- card -->
+                        <div class="card">
+                            <h2>Answering phone calls</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis odio</p>
+                        </div>
+                        <!-- card -->
+                        <div class="card">
+                            <h2>Basic data entry</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis odio</p>
+                        </div>
+                        <!-- card -->
+                        <div class="card">
+                            <h2>Organizing calendar</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis odio</p>
+                        </div>
+                        <!-- card -->
+                        <div class="card">
+                            <h2>Booking travels</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis odio</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <section id="learnMore" class="bg_blue">'Learn-more' section</section>
         <section>'brands' section</section>
     </main>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
 section {
     min-height: 1000px;
+}
+
+//SERVICES SECTION
+
+.services {
+    color: $orange;
+    padding: 25px 0;
+    border-bottom: solid 2px $lightGray ;
+}
+
+.servicesCards {
+    display: flex;
+
+
+    .card {
+        width: 25%;
+        padding: 60px;
+        border-right: solid 2px $lightGray;
+
+        &:last-child {
+            border-right: none;
+        }
+    }
 }
 
 #learnMore {
