5eb27b153e7cf88ef4133e30fd9c1015b4053ddb
CarloSalaris <carloa.salaris@gmail.com>
2023-06-14 17:21:03 +0200

    Fixed side squares

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -9,6 +9,7 @@ export default {
 
 <template>
     <div class="jumbo bg_blue">
+
         <!-- LEFT BLOCK -->
         <div class="container_centered">
             <div class="block">
@@ -31,10 +32,23 @@ export default {
             <div class="pic_container">
                 <!-- IMAGE - Bottom out of background -->
                 <img src="../assets/images/pics/info-5-800x887.jpg" alt="">
-
-
             </div>
         </div>
+
+        <!-- Fixed elements -->
+        <div class="sideSquares">
+            <!-- square -->
+            <div class="sideSquare">
+                <i class="fa-regular fa-folder-open"></i>
+                <div>Demos</div>
+            </div>
+            <!-- square -->
+            <div class="sideSquare">
+                <div class="offer">$39</div>
+                <div>On Sale</div>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -46,6 +60,7 @@ export default {
     background-image: url("../assets/images/bg/home-bg-3.svg");
     background-size: cover;
     background-position: center;
+    position: relative;
 }
 
 .container_centered {
@@ -68,5 +83,39 @@ export default {
 .pic_container {
     width: 45%;
     max-height: 570px;
+}
+
+.sideSquares {
+    position: fixed;
+    top: 110px;
+    right: 10px;
+
+    .sideSquare {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 55px;
+        height: 55px;
+        background-color: #ffffff;
+        color: $iconGray;
+        padding: 5px;
+        border: 1px solid;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        text-align: center;
+        font-size: .7rem;
+        cursor: pointer;
+
+        i {
+            font-size: 1.3rem;
+        }
+
+        .offer {
+            color: $iconGreen;
+            font-size: 1.2rem;
+            font-weight: 500;
+        }
+    }
 }
 </style>
